54e2ed143847585bc8c7ddd1c4e303bd8eda9083
jia xiaopeng <snailkn@sina.com>
2023-06-09 13:21:51 +0800

    edit

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,83 +1,87 @@
 <script setup>
-import { RobotOutlined } from "@ant-design/icons-vue";
-import { useRoute } from "vue-router";
+import {RobotOutlined} from "@ant-design/icons-vue";
+import {useRoute} from "vue-router";
 import axios from "axios";
-import { reactive } from "vue";
+import {reactive} from "vue";
 
 const route = useRoute();
 const demand = route.query.demand;
 
 const state = reactive({
-  suggestion: {},
+    suggestion: {},
+    loading: true
 });
-// let suggestion = reactive(null);
-// const updateKey = ref(0)
 
-// const suggestion = {
-//     features: [
-//         { label: "品牌", value: "联想" },
-//         { label: "型号", value: "00001" },
-//         { label: "内存", value: "16G" },
-//         { label: "重量", value: "1.5 Kg" },
-//         { label: "尺寸", value: "300 * 150 * 32 mm" },
-//     ],
-//     reason: "推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因推荐原因\n"
-// }
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
-  timeout: 5000,
+    baseURL: "https://localhost:26913",
+    timeout: 500000,
 });
 instance
-  .get("/chat", {
-    params: { demand: demand },
-  })
-  .then(function (response) {
-    // 处理成功情况
-    state.suggestion = response.data;
-    // console.log(suggestion)
-    // return suggestion
-    //     updateKey.value += 1
-    // console.log(updateKey.value)
-  })
-  .catch();
+    .get("/chat", {
+        params: {demand: demand},
+    })
+    .then(function (response) {
+        // 处理成功情况
+        state.suggestion = response.data;
+        state.loading = false;
+    })
+    .catch();
 </script>
 
 <template>
-  <a-row style="margin-top: 50px">
-    <a-col :span="4"></a-col>
-    <a-col :span="16">
-      <div style="display: flex">
-        <a-avatar :size="64" style="background-color: #1890ff">
-          <template #icon>
-            <robot-outlined />
-          </template>
-        </a-avatar>
-        <div class="h1" style="color: black; margin-left: 25px">
-          以下是为您推荐的产品，供您参考
-        </div>
-      </div>
-      <a-card style="margin-top: 20px">
-        <a-descriptions title="产品信息">
-          <a-descriptions-item
-            v-for="feat in state.suggestion.features"
-            :label="feat.label"
-          >
-            {{ feat.value }}
-          </a-descriptions-item>
-        </a-descriptions>
-        <a-descriptions title="推荐原因" style="margin-top: 25px">
-          <a-descriptions-item>{{
-            state.suggestion.reason
-          }}</a-descriptions-item>
-        </a-descriptions>
-      </a-card>
-    </a-col>
-    <a-col :span="4"></a-col>
-  </a-row>
+    <a-row style="margin-top: 50px">
+        <a-col :span="4"></a-col>
+        <a-col :span="16">
+            <div style="display: flex">
+                <a-avatar :size="64" style="background-color: #1890ff">
+                    <template #icon>
+                        <robot-outlined/>
+                    </template>
+                </a-avatar>
+                <div class="h1" style="color: black; margin-left: 25px">
+                    以下是为您推荐的产品，供您参考
+                </div>
+            </div>
+            <a-card v-if="state.loading" class="loading">
+                <a-spin size="large"/>
+                <h3 style="margin-top: 20px">正在为您推荐，请耐心等待</h3>
+            </a-card>
+            <a-card style="margin-top: 20px" v-if="!state.loading">
+                <a-descriptions title="用户诉求" style="margin-top: 25px">
+                    <a-descriptions-item>{{
+                        demand
+                        }}
+                    </a-descriptions-item>
+                </a-descriptions>
+                <a-descriptions title="产品信息">
+                    <a-descriptions-item
+                            v-for="feat in state.suggestion.features"
+                            :label="feat.label"
+                    >
+                        {{ feat.value }}
+                    </a-descriptions-item>
+                </a-descriptions>
+                <a-descriptions title="推荐原因" style="margin-top: 25px">
+                    <a-descriptions-item>{{
+                        state.suggestion.reason
+                        }}
+                    </a-descriptions-item>
+                </a-descriptions>
+            </a-card>
+        </a-col>
+        <a-col :span="4"></a-col>
+    </a-row>
 </template>
 
 <style scoped>
 .h1 {
-  font-size: x-large;
+    font-size: x-large;
+}
+.loading {
+    text-align: center;
+    border-radius: 4px;
+    margin-bottom: 20px;
+    padding: 30px 50px;
+    margin: 20px 0;
 }
 </style>
